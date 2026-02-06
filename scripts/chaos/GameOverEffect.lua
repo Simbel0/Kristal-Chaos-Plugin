fbd92b7d997ff3b71af10580babe714d7205fc0a
Simbel <61120464+Simbel0@users.noreply.github.com>
2026-02-06 02:54:29 +0100

    GameOverEffect tries to place the soul at the right place

--- scripts/chaos/GameOverEffect.lua
+++ scripts/chaos/GameOverEffect.lua
@@ -1,7 +1,17 @@
 local GameOverEffect, super = Class(ChaosEffect, "gameover_effect")
 
 function GameOverEffect:onEffectStart(in_battle)
-	Game:gameOver()
+	local soul_x, soul_y
+	if in_battle then
+		if Game.battle.soul then
+			soul_x, soul_y = Game.battle.soul:getPosition()
+		else
+			soul_x, soul_y = Game.battle.party[1]:getPosition()
+		end
+	elseif Game.world and Game.world.player then
+		soul_x, soul_y = Game.world.player:getPosition()
+	end
+	Game:gameOver(soul_x, soul_y)
 end
 
 return GameOverEffect
